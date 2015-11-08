0106f392a807114adeec02c4d38587ffec84ee60
Chaitya Shah <chaityabshah@verizon.net>
2015-11-07 20:10:17 -0500

    Add Google API Use

--- app.js
+++ app.js
@@ -133,6 +133,10 @@ app.get('/auth/facebook/callback', passport.authenticate('facebook', { failureRe
   res.redirect(req.session.returnTo || '/');
 });
 
+app.get('/auth/google', passport.authenticate('google', { scope: 'profile email' }));		
+app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/login' }), function(req, res) {		
+  res.redirect(req.session.returnTo || '/');		
+});
 /**
  * Error Handler.
  */
