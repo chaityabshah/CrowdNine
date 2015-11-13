4c399a99a8b7af42748ca3239045920ba023e8b3
Chaitya Shah <chaityabshah@verizon.net>
2015-11-13 13:36:45 -0500

    update facebook api

--- config/secrets.js
+++ config/secrets.js
@@ -50,8 +50,8 @@ module.exports = {
   },
 
   facebook: {
-    clientID: process.env.FACEBOOK_ID || '754220301289665',
-    clientSecret: process.env.FACEBOOK_SECRET || '41860e58c256a3d7ad8267d3c1939a4a',
+    clientID: process.env.FACEBOOK_ID || '1203669079649647',
+    clientSecret: process.env.FACEBOOK_SECRET || 'f72e831ebfe959729bc673f04376050c',
     callbackURL: '/auth/facebook/callback',
     passReqToCallback: true
   },
